9759a3fec50cc5afe48867a332f817c014dc6dd2
sreeduvarakaqsnevens@gmail.com <sreeduvaraka2003@gmail.com>
2024-12-25 10:47:56 +0530

    sending a new folder

---
+++ fonts/flaticon/flaticon-icons.js
@@ -0,0 +1,33 @@
+{
+"icons": [     
+"flaticon-bathroom",
+"flaticon-blueprint",
+"flaticon-bridge-saw",
+"flaticon-chisel",
+"flaticon-construction",
+"flaticon-dining-table",
+"flaticon-floor",
+"flaticon-hammer",
+"flaticon-living-room",
+"flaticon-marbles",
+"flaticon-modern-art",
+"flaticon-panels",
+"flaticon-stone",
+"flaticon-stones",
+"flaticon-suckers",
+"flaticon-tile-1",
+"flaticon-tile-2",
+"flaticon-tile",
+"flaticon-tiles-1",
+"flaticon-tiles-2",
+"flaticon-tiles-3",
+"flaticon-tiles",
+"flaticon-trowel",
+"flaticon-wood"
+
+    ]
+  }
+
+
+
+
